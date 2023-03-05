83a03fb377bc8a7ddbcbbd1f1a9fc6e1a83993c2
ellsphillips <elliott.phillips.dev@gmail.com>
2023-03-05 16:19:57 +0000

    Feature: clean up post build projectiles

--- linkedincv/cli/command.py
+++ linkedincv/cli/command.py
@@ -1,11 +1,18 @@
 import subprocess
+from distutils.dir_util import copy_tree
 from pathlib import Path
 
 
 def build(tex_script_path: Path, out_folder: Path) -> None:
     COMMAND = f"""
         xelatex -synctex=1 -interaction=nonstopmode {tex_script_path}
-        mv main.pdf {out_folder}/
     """
-    Path(out_folder).mkdir(parents=True, exist_ok=True)
+    out_folder.mkdir(parents=True, exist_ok=True)
+    copy_tree(str(tex_script_path.parent), str(out_folder))
     subprocess.run(COMMAND, shell=True, check=True)
+
+    for projectile in Path(".").glob("main*"):
+        if projectile.suffix == ".pdf":
+            projectile.rename(out_folder / projectile.name)
+        else:
+            projectile.unlink()
